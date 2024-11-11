9931da8f2113cda22c57f3f3b70951f8a69dca89
lukabagash <bagash_l2@denison.edu>
2024-11-10 23:32:44 -0500

    Add two additional parameters

--- src/app/simulation/components/SimulationControls.tsx
+++ src/app/simulation/components/SimulationControls.tsx
@@ -14,6 +14,8 @@ type SimulationControlsProps = {
   speed: number;
   handleSpeedChange: () => void;
   initialInfected: number;
+  contactRange: [number, number]; // New parameter
+  areaSize: number; // New parameter
 };
 
 export default function SimulationControls({
@@ -28,14 +30,16 @@ export default function SimulationControls({
   populationSize,
   speed,
   handleSpeedChange,
-  initialInfected
+  initialInfected,
+  contactRange,
+  areaSize,
 }: SimulationControlsProps) {
   return (
     <div>
       <h2>Flu Simulation</h2>
       <p>Vaccine Efficacy: {Math.round(vaccineEfficacy * 100)}%</p>
       <p>Vaccination Rate: {Math.round(vaccinationRate * 100)}%</p>
-      <p>Inital Infected: {Math.round(initialInfected * 100)}%</p>
+      <p>Initial Infected: {Math.round(initialInfected * 100)}%</p>
       <p>R0 (Infection Rate): {R0}</p>
       <p>Contagious Factor (Isolated): {contagiousFactorForIso}</p>
       <p>Contagious Factor (Unisolated): {contagiousFactorForUniso}</p>
@@ -43,6 +47,10 @@ export default function SimulationControls({
       <p>Recovery Rate: {Math.round(recoveryRate * 100)}%</p>
       <p>Days: {days}</p>
       <p>Population Size: {populationSize}</p>
+      <p>
+        Contact Range: {contactRange[0]} - {contactRange[1]}
+      </p>
+      <p>Area Size: {areaSize} units</p>
       <button onClick={handleSpeedChange}>
         {speed === 1
           ? "Speed Up (2x)"
