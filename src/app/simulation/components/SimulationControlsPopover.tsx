0bb71310baa9799f236d0d25c2ddd8076af0a472
Ichhit Joshi <joshi_i1@denison.edu>
2024-11-13 14:51:32 -0500

    updated Add New Simulation info hover

--- src/app/simulation/components/SimulationControlsPopover.tsx
+++ src/app/simulation/components/SimulationControlsPopover.tsx
@@ -10,6 +10,21 @@ import {
 } from "@/components/ui/popover";
 import { Plus } from "lucide-react";
 import { SimulationParameters, defaultParameters } from "../../simulationParameters";
+import { Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip";
+import { Info } from "lucide-react";
+
+const tooltips = {
+  vaccineEfficacy: "Effectiveness of the vaccine in preventing infection, reducing the infection rate in vaccinated individuals.",
+  vaccinationRate: "Proportion of the population that is vaccinated, which affects susceptibility.",
+  initialInfected: "Initial count of infected individuals at the start. Sets the starting point of the infection curve.",
+  R0: "Basic reproduction number, representing the average number of people one infected individual will infect if no one is immune.",
+  isolationRate: "Proportion of the population that isolates to reduce infection spread. Lower isolation leads to higher infection spread.",
+  recoveryRate: "Rate at which infected individuals recover each day (proportion per day).",
+  days: "Duration of the simulation in days",
+  populationSize: "Total population size in the simulation. Affects the overall scale of the outbreak.",
+  contactRange: "The range within which an infected person can spread the disease.",
+  areaSize:"N/A"
+};
 
 interface SimulationControlsPopoverProps {
   onStartSimulation: (parameters: SimulationParameters) => void;
@@ -53,9 +68,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
           <div style={{ display: "grid", gap: "16px", marginTop: "20px" }}>
             {/* Vaccine Efficacy */}
             <div>
-              <label>
-                Vaccine Efficacy ({Math.round(parameters.vaccineEfficacy * 100)}%)
-              </label>
+              <div style={{ display: "flex", alignItems: "center" }}>
+              <label>Vaccine Efficacy ({Math.round(parameters.vaccineEfficacy * 100)}%)</label>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+                </TooltipTrigger>
+                <TooltipContent>{tooltips.vaccineEfficacy}</TooltipContent>
+              </Tooltip>
+              </div>
               <input
                 type="range"
                 min="0"
@@ -71,9 +92,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Vaccination Rate */}
             <div>
-              <label>
-                Vaccination Rate ({Math.round(parameters.vaccinationRate * 100)}%)
-              </label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Vaccination Rate ({Math.round(parameters.vaccinationRate * 100)}%)</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.vaccinationRate}</TooltipContent>
+            </Tooltip>
+            </div>
               <input
                 type="range"
                 min="0"
@@ -89,9 +116,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Initial Infected */}
             <div>
-              <label>
-                Initial Infected ({Math.round(parameters.initialInfected * 100)}%)
-              </label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Initial Infected ({Math.round(parameters.initialInfected * 100)}%)</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.initialInfected}</TooltipContent>
+            </Tooltip>
+          </div>
               <input
                 type="range"
                 min="0"
@@ -107,7 +140,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* R0 */}
             <div>
-              <label>R0 (Infection Rate): {parameters.R0}</label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>R0 (Infection Rate): {parameters.R0}</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.R0}</TooltipContent>
+            </Tooltip>
+          </div>
               <input
                 type="number"
                 min="0.5"
@@ -128,9 +169,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Isolation Rate */}
             <div>
-              <label>
-                Isolation Rate ({Math.round(parameters.isolationRate * 100)}%)
-              </label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Isolation Rate ({Math.round(parameters.isolationRate * 100)}%)</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.isolationRate}</TooltipContent>
+            </Tooltip>
+          </div>
               <input
                 type="range"
                 min="0"
@@ -146,9 +193,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Recovery Rate */}
             <div>
-              <label>
-                Recovery Rate ({Math.round(parameters.recoveryRate * 100)}%)
-              </label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Recovery Rate ({Math.round(parameters.recoveryRate * 100)}%)</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.recoveryRate}</TooltipContent>
+            </Tooltip>
+          </div>
               <input
                 type="range"
                 min="0.01"
@@ -164,7 +217,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Days */}
             <div>
-              <label>Days ({parameters.days})</label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Days ({parameters.days})</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.days}</TooltipContent>
+            </Tooltip>
+          </div>
               <input
                 type="number"
                 min="1"
@@ -184,7 +245,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Population Size */}
             <div>
-              <label>Population Size ({parameters.populationSize})</label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Population Size ({parameters.populationSize})</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.populationSize}</TooltipContent>
+            </Tooltip>
+          </div>
               <input
                 type="number"
                 min="50"
@@ -205,9 +274,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Contact Range */}
             <div>
-              <label>
-                Contact Range: {parameters.contactRange[0]} - {parameters.contactRange[1]}
-              </label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Contact Range: {parameters.contactRange[0]} - {parameters.contactRange[1]}</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.contactRange}</TooltipContent>
+            </Tooltip>
+          </div>
               <div style={{ display: "flex", gap: "8px" }}>
                 <input
                   type="number"
@@ -250,7 +325,15 @@ const SimulationControlsPop: React.FC<SimulationControlsPopoverProps> = ({
 
             {/* Area Size */}
             <div>
-              <label>Area Size ({parameters.areaSize} units)</label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+            <label>Area Size ({parameters.areaSize} units)</label>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info style={{ marginLeft: "8px", cursor: "pointer" }} size={16} />
+              </TooltipTrigger>
+              <TooltipContent>{tooltips.areaSize}</TooltipContent>
+            </Tooltip>
+          </div>
               <input
                 type="number"
                 min="10"
